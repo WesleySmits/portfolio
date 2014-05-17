469619495518e14b861f19890fb70cdd92f23e7a
WesleySmits <wesleysmits@gmail.com>
2014-05-17 16:09:40 +0200

    Fixed paperclip bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516125454) do
+ActiveRecord::Schema.define(version: 20140517115736) do
 
 # Could not dump table "articles" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
