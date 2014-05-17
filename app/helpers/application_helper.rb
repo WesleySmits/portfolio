ff866d6be4b2c7953ff32f20109106e5b6e4c413
WesleySmits <wesleysmits@gmail.com>
2014-05-17 13:22:57 +0200

    Added two helper functions for applying markdown and syntax highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,11 +6,25 @@ module ApplicationHelper
       content_for?(:title) ? APP_CONFIG['default_title'] + ' | ' + content_for(:title) : APP_CONFIG['default_title']
     end
   end
+  
   def meta_description(desc = nil)
     if desc.present?
       content_for :meta_description, desc
     else
       content_for?(:meta_description) ? content_for(:meta_description) : APP_CONFIG['meta_description']
     end
+  end
+
+  def markdown(text)
+    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
+    syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
+  end
+  
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
   end
 end
